7e21725e2f60a56e56d678da240b9aa8982fc785
Jaya Varsini <jayavarsini007@gmail.com>
2023-08-16 00:03:43 +0530

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
+import NavBar from './navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to the react blog";
-  const likes = 100;
-  const link = "https://www.google.com/";
-
   return (
     <div className="App">
+      <NavBar />
     <div className="content">
-      <h1> {title} </h1>
-      <p>Liked {likes}k times. </p>
-      <p> {10} </p>
-      <p> {"Hello, World"} </p>
-      <p> { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] } </p>
-      <p> {Math.random() * 10} </p>
-      <a href={link}>Google</a>
+      <Home />
       </div>
     </div>
   );
