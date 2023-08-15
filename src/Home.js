2514fb5cd17f42db5056e5f13c51d35d9fe9f4af
Jaya Varsini <jayavarsini007@gmail.com>
2023-08-16 00:49:02 +0530

    Click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const toClick = () => {
+        console.log("Hello, Viewer");
+    };
+
+    const toClickAgain = (name) => {
+        console.log("Hello, " + name);
+    };
+
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={toClick}>Click here</button>
+            <button onClick={() => toClickAgain("Varsi")}>Click here again</button>
         </div>
     );
 }
