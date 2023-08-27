6404943d0ebb2035200d60ec9386138ee07f8236
Jaya Varsini <jayavarsini007@gmail.com>
2023-08-27 15:10:58 +0530

    Loading message

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,24 @@ import BlogList from "./BlogsList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
     
     useEffect(()=> {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
             })
             .then(data => {
-                console.log(data);
                 setBlogs(data);
-            });
+                setIsPending(false);
+            })
+        }, 1000);
     }, []);
     
     return (
         <div className="home">
+            { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogList ={blogs} title= "All Blogs"/> }
         </div>
     );
